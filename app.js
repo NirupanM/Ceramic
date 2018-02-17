3510d66ea48daae3af917bb2428332cef1379a9f
NirupanM <nirupanm007@gmail.com>
2018-02-17 22:46:01 +0530

    email testing cycle10

--- app.js
+++ app.js
@@ -113,7 +113,6 @@ var cartArray = window.sessionStorage["itemscart"];
 var messageString = $('#name_input').val();
 
 
-// $('#mail-message').val("\nCustomer Details:"+"\n"+"Name:"+$('#name_input').val()+"\n"+"Email:"+$('#email_input').val()+"\n"+"Phone:"+$('#phone_input').val()+"\n"+"Address:"+$('#address_input').val());
 
 
 $('#mail-message').val("\nCustomer Details:"+"\n"+"Name:"+$('#name_input').val()+"\n"+"Email:"+$('#email_input').val()+"\n"+"Phone:"+$('#phone_input').val()+"\n"+"Address:"+$('#address_input').val());
