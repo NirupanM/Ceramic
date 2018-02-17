e7d82f04e897dab906ee7f49009cbe4b491e36e8
NirupanM <nirupanm007@gmail.com>
2018-02-17 20:45:07 +0530

    email testing cycle6

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ $("#form-save").click(function(){
 
 var messageString = $('#name_input').val();
 
-$('#mail-message').val("\nCustomer Details:"+"\n"+"Name:"+$('#name_input').val()+"\n"+"Email:"$('#email_input').val()+"\n"+"Phone:"+$('#phone_input').val()+"\n"+"Address:"+$('#address_input').val()
 
-"\nOrdered Items");
+
+('#mail-message').val("\nCustomer Details:"+"\n"+"Name:"+$('#name_input').val()+"\n"+"Email:"+$('#email_input').val()+"\n"+"Phone:"+$('#phone_input').val()+"\n"+"Address:"+$('#address_input').val());
 
 
 
